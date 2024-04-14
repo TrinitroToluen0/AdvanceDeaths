b1d3949bb54176c4341846332d98402a865d4a0d
TrinitroToluen0 <javiermenco404@gmail.com>
2024-04-13 23:56:52 -0500

    Update translationContainer.php

--- src/ErikPDev/AdvanceDeaths/utils/translationContainer.php
+++ src/ErikPDev/AdvanceDeaths/utils/translationContainer.php
@@ -4,33 +4,27 @@ namespace ErikPDev\AdvanceDeaths\utils;
 
 use ErikPDev\AdvanceDeaths\ADMain;
 
-class translationContainer {
+class translationContainer
+{
 
 	private static array $translations;
 
-	public function __construct() {
+	public function __construct()
+	{
 
 		self::$translations = yaml_parse_file(ADMain::getInstance()->getDataFolder() . "lang.yml");
-
 	}
 
-	public static function translate(string $translationString, bool $showPrefix, array $params): array|string {
-
-		$prefix = match ($showPrefix) {
-			true => "", // Make this customizable in config ??
-			default => "",
-		};
-
+	public static function translate(string $translationString, bool $showPrefix, array $params): array|string
+	{
 		if (!array_key_exists($translationString, self::$translations)) return "null";
 
-		$translated = $prefix . " " . self::$translations[$translationString];
+		$translated = self::$translations[$translationString];
 
 		foreach ($params as $transKey => $transValue) {
 			$translated = str_replace("%" . $transKey, $transValue, $translated);
 		}
 
 		return $translated;
-
 	}
-
 }
